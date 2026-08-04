06029fdaadd67d19adf51f973bc6c9e38575b322
marmooo <marmooo@users.noreply.github.com>
2026-08-05 03:20:46 +0900

    bump @marmooo/midi-player from 0.0.7 to 0.0.8

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { Midy } from "https://cdn.jsdelivr.net/gh/marmooo/midy@0.6.0/dist/midy.min.js";
-import { MIDIPlayer } from "https://cdn.jsdelivr.net/npm/@marmooo/midi-player@0.0.7/+esm";
+import { MIDIPlayer } from "https://cdn.jsdelivr.net/npm/@marmooo/midi-player@0.0.8/+esm";
 import { Modal } from "https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/+esm";
 import { MidiLibrary } from "/free-midi/midi-library.js";
 
